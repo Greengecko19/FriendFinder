f7df290ecc50a6a369d3e721a2768fc58d34356c
Kamran Grasselli <kfg09@yahoo.com>
2018-10-21 00:19:01 -0400

    Basic shell, no data working

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 
 var app = express();
-
 var PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
 
 
 require("./routes/apiRoutes")(app);
